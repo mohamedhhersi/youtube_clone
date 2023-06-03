3991cfd00da46eaa909ff1ed5917ec9945223f2a
Mohamed Hersi <mohamed.hersi1232@gmail.com>
2023-06-03 07:24:13 -0400

    implemented yt api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,27 +1,49 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
-const Feed = () => (
-  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-    <Box
-      sx={{
-        height: { sx: "auto", md: "92vh" },
-        borderRight: "1px solid #3d3d3d",
-        px: { sx: 0, md: 2 },
-      }}
-    >
-      <Sidebar />
+const Feed = () => {
+  const [selectedCategory, setselectedCategory] = useState("New");
 
-      <Typography
-        className="copyright"
-        variant="body2"
-        sx={{ mt: 1.5, color: "#fff" }}
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
       >
-        Copyright 2023 Mohamed Hersi
-      </Typography>
-    </Box>
-  </Stack>
-);
+        <Sidebar />
+
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 Mohamed Hersi
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed;
