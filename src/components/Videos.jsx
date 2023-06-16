dc224e2cb6dc04e20614aeee9a788aa2332bd310
Mohamed Hersi <mohamed.hersi1232@gmail.com>
2023-06-16 01:05:02 -0400

    worked on channelcard component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,12 +5,12 @@ const Videos = ({ videos }) => {
   console.log(videos);
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item, idx) => {
-        <Box key={{ idx }}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
-        </Box>;
-      })}
+        </Box>
+      ))}
     </Stack>
   );
 };
