b18eaa106385ca7c4e257caae14c16d319b6f19c
Mohamed <mohamed.hersi1232@gmail.com>
2023-07-12 07:04:28 -0400

    worked on SearchFeed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,10 +3,27 @@ import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
 
-const SearchBar = () => (
+const SearchBar = () => {
+
+const [searchTerm, setsearchTerm] = useState('')
+const navigate = useNavigate()
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  
+  if(searchTerm) {
+    navigate(`/search/${searchTerm}`)
+  
+    setsearchTerm('')
+  }
+}
+
+
+
+  return (
   <Paper
     component="form"
-    onSubmit={() => {}}
+    onSubmit={(handleSubmit) => {}}
     sx={{
       borderRadius: 20,
       border: "1px solid #e3e3e3",
@@ -18,13 +35,15 @@ const SearchBar = () => (
     <input
       className="search-bar"
       placeholder="Search..."
-      value=""
-      onChange={() => {}}
+      value={searchTerm}
+      onChange={(e) => {
+        setsearchTerm(e.target.value)
+      }}
     />
     <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
       <Search />
     </IconButton>
-  </Paper>
-);
+  </Paper>) 
+};
 
 export default SearchBar;
