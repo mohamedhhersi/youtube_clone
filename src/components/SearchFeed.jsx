a8125afb4f9dd69485996bf20523da09e4857037
Mohamed Hersi <mohamed.hersi1232@gmail.com>
2023-07-14 06:10:13 -0400

    worked on search feed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -2,32 +2,27 @@ import { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import {useParams} from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
-
   const [videos, setVideos] = useState([]);
-  const {searchTerm} = useParams()
+  const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q={searchTerm}`).then((data) => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
       setVideos(data.items);
     });
   }, [searchTerm]);
 
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-    <Typography
-      variant="h4"
-      fontWeight="bold"
-      mb={2}
-      sx={{ color: "white" }}
-    >
-      Search Results for: <span style={{ color: "#F31503" }}>{searchTerm}</span>
-    </Typography>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for:{" "}
+        <span style={{ color: "#F31503" }}>{searchTerm}</span> videos
+      </Typography>
 
-    <Videos videos={videos} />
-  </Box>
+      <Videos videos={videos} />
+    </Box>
   );
 };
 
