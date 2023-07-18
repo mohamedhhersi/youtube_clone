ac69f963c64f39b4f20c3467376faffee6c0210b
Mohamed Hersi <mohamed.hersi1232@gmail.com>
2023-07-18 05:37:22 -0400

    worked on related videos section

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -3,7 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
-import { Video } from "./";
+import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
@@ -73,6 +73,14 @@ const VideoDetail = () => {
               </Stack>
             </Stack>
           </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
     </Box>
