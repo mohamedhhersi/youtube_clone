9c894f4b5e8ca063170e769a3636fb31a74de464
Mohamed <mohamed.hersi1232@gmail.com>
2023-07-17 20:59:51 -0400

    worked on vid detail component

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -8,14 +8,23 @@ import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
   const { id } = useParams();
 
   // const { snippet: { title } = {} } = videoDetail || {};
   const title = videoDetail?.snippet?.title ?? "";
+  const channelId = videoDetail?.snippet?.channelId ?? "";
+  const channelTitle = videoDetail?.snippet?.channelTitle ?? "";
+  const viewCount = videoDetail?.statistics.viewCount ?? "";
+  const likeCount = videoDetail?.statistics.likeCount ?? "";
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
+    );
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
     );
   }, [id]);
 
@@ -34,6 +43,35 @@ const VideoDetail = () => {
                 {title}
               </Typography>
             )}
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: "#fff" }}
+              py={1}
+              px={2}
+            >
+              <Link to={`/channel/${channelId}`}>
+                <Typography
+                  variant={{ sm: "subtitle1", md: "h6" }}
+                  color="#fff"
+                >
+                  {channelTitle}
+                  <CheckCircle
+                    sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+                  />
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" sx={{ opacity: "0.7" }}>
+                  {" "}
+                  {parseInt(viewCount).toLocaleString()} views{" "}
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: "0.7" }}>
+                  {" "}
+                  {parseInt(likeCount).toLocaleString()} likes{" "}
+                </Typography>
+              </Stack>
+            </Stack>
           </Box>
         </Box>
       </Stack>
